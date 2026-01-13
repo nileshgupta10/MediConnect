9962911ee35f4fa7bcb90ef5c0dd7015582a705f
nileshgupta10 <nileshguptashiv@gmail.com>
2026-01-13 18:00:05 +0530

    Allow public home page without layout/auth

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,18 @@
 import Layout from '../components/Layout';
+import { useRouter } from 'next/router';
 
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  // Pages that should NOT use Layout
+  const noLayoutPages = ['/', '/simple-login', '/auth-callback'];
+
+  const isNoLayout = noLayoutPages.includes(router.pathname);
+
+  if (isNoLayout) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <Layout>
       <Component {...pageProps} />
